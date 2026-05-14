51a0b686fea0e6bb85d8be976ac1ad325692cb9b
VcteAlbero <valbero@upv.es>
2026-05-14 16:18:21 +0200

    nuevas talas para descarga y ocultamos deformada

--- appForjados.py
+++ appForjados.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Título de la aplicación
-st.title("Cálculo de Forjados")
+st.title("Forjados: Análisis lineal")
 
 # Entrada de datos en sidebar
 st.sidebar.header("Datos de Entrada")
@@ -21,7 +21,7 @@ flagCarga = 1 if radio_option == "SUP" else 0
 radio_option = st.sidebar.radio("Tipo de cálculo:", ["CONT", "ISO"])
 flagCONT = 1 if radio_option == "CONT" else 0
 #
-EI = st.sidebar.number_input("RIGIDEZ (kNm2):", min_value=1, value=16800)
+#EI = st.sidebar.number_input("RIGIDEZ (kNm2):", min_value=1, value=16800)
 
 # Generar una tabla editable en función de n vanos
 st.write("### Introduce los datos en la tabla:")
@@ -54,7 +54,7 @@ else:
 MApoyos = calculosForjados.clapeyron(luces, flagCarga, carga_sup, carga_punt, x_punt, libre_izq, libre_der, flagCONT)
 ley_momentos = calculosForjados.Ley_M(luces, flagCarga, carga_sup, carga_punt, x_punt, libre_izq, libre_der, MApoyos)
 ley_cortantes = calculosForjados.Ley_V(luces, flagCarga, carga_sup, carga_punt, x_punt, libre_izq, libre_der, MApoyos)
-deformacion = calculosForjados.deformada(luces, ley_momentos, EI, libre_izq, libre_der)
+#deformacion = calculosForjados.deformada(luces, ley_momentos, EI, libre_izq, libre_der)
 
 # Ley de momento flectores
 st.write("### 1. Ley de momentos flectores:")
@@ -82,17 +82,35 @@ plt.legend(fontsize=12)
 st.pyplot(fig2)
 
 # Deformada
-st.write("### 3. Deformada:")
-distanciaD = [i / 100 for i in range(len(deformacion))]
-fig3 = plt.figure(figsize=(10, 1.5))
-plt.plot(distanciaD, deformacion, label="Deformada", color="black", linewidth=2)
-plt.title("Deformada a lo largo de la estructura", fontsize=14)
-plt.xlabel("Distancia (m)", fontsize=12)
-plt.ylabel("Deformación (mm)", fontsize=12)
-plt.axhline(0, color="black", linewidth=0.8, linestyle="--")  # Línea de referencia en 0
-plt.grid(True, linestyle="--", alpha=0.6)
-plt.legend(fontsize=12)
-st.pyplot(fig3)
+# st.write("### 3. Deformada:")
+# distanciaD = [i / 100 for i in range(len(deformacion))]
+# fig3 = plt.figure(figsize=(10, 1.5))
+# plt.plot(distanciaD, deformacion, label="Deformada", color="black", linewidth=2)
+# plt.title("Deformada a lo largo de la estructura", fontsize=14)
+# plt.xlabel("Distancia (m)", fontsize=12)
+# plt.ylabel("Deformación (mm)", fontsize=12)
+# plt.axhline(0, color="black", linewidth=0.8, linestyle="--")  # Línea de referencia en 0
+# plt.grid(True, linestyle="--", alpha=0.6)
+# plt.legend(fontsize=12)
+# st.pyplot(fig3)
+
+# Tabla para MApoyos
+st.write("### 3. Momentos en Apoyos:")
+df_mapoyos = pd.DataFrame({
+    "Apoyo": range(1, len(MApoyos) + 1),
+    "Momento (kNm)": MApoyos
+})
+st.dataframe(df_mapoyos)
+
+# Tabla para ley_momentos, ley_cortantes
+st.write("### 4. Tabla de Resultados:")
+distancia = [i / 100 for i in range(len(ley_momentos))]
+df_results = pd.DataFrame({
+    "Distancia (m)": distancia,
+    "Momentos (kNm)": ley_momentos,
+    "Cortantes (kN)": ley_cortantes,
+})
+st.dataframe(df_results)
 
 # Pie de página con información de contacto
 st.markdown(
@@ -100,8 +118,8 @@ st.markdown(
     <hr style="border:1px solid gray;margin-top:50px;">
     <footer>
         <p style="text-align:center; font-size:12px; color:gray;">
-        Esta es una aplicación en pruebas. Si tienes comentarios o preguntas, por favor contacta con: 
-        <a href="mailto:valbero@uji.es">valbero@upv.es</a>.
+        Esta es una aplicación docente, no comercial. Si tienes comentarios o preguntas, por favor contacta con: 
+        <a href="mailto:valbero@upv.es">valbero@upv.es</a>.
         </p>
     </footer>
     """,
